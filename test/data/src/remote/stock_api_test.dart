c98521e9092c5a38f4d38bb8c21bbd98dc74d6bf
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-10-14 21:03:50 +0900

    test

--- test/data/src/remote/stock_api_test.dart
+++ test/data/src/remote/stock_api_test.dart
@@ -1,29 +1,49 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:stock_app/core/utils/constant/config.dart';
-import 'package:stock_app/core/utils/injections.dart';
+import 'package:stock_app/core/utils/constant/network_constant.dart';
+import 'package:stock_app/core/utils/log/app_logger.dart';
 import 'package:stock_app/data/csv/company_listings_parser.dart';
 import 'package:stock_app/data/src/remote/stock_api.dart';
 
+import '../../../dummy.dart';
+import 'stock_api_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<Dio>()])
 void main() {
-  late Dio dio = Dio();
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late MockDio mockDio;
 
   setUp(() async {
-    dio = Dio();
-    await initSettings();
+    mockDio = MockDio();
+    initRootLogger();
+    await dotenv.load(fileName: ".env");
   });
 
   group('Stock Api Test', () {
     test('네트워크 통신', () async {
       final parser = CompanyListingsParser();
 
-      final response = await StockApi(dio: dio).getListing(Config.stockApiKey);
+      when(mockDio.get(getListingsPath())).thenAnswer(
+        (_) async => Response(
+          requestOptions: RequestOptions(),
+          statusCode: 200,
+          data: Dummy.dummyList,
+        ),
+      );
+
+      final response = await StockApi(
+        dio: mockDio,
+      ).getListing(Config.stockApiKey);
 
       final remoteListings = await parser.parse(response.data);
 
-      expect(remoteListings[1].symbol, 'A');
-      expect(remoteListings[1].name, 'Agilent Technologies Inc');
-      expect(remoteListings[1].exchange, 'NYSE');
+      expect(remoteListings[0].symbol, 'A');
+      expect(remoteListings[0].name, 'Agilent Technologies Inc');
+      expect(remoteListings[0].exchange, 'NYSE');
     });
   });
 }
--- test/dummy.dart
+++ test/dummy.dart
@@ -1,1 +1,14 @@
-
+abstract class Dummy {
+  static const String dummyList = '''
+A,Agilent Technologies Inc,NYSE,Stock,1999-11-18,null,Active
+AA,Alcoa Corp,NYSE,Stock,2016-10-18,null,Active
+AAA,ALTERNATIVE ACCESS FIRST PRIORITY CLO BOND ETF ,NYSE ARCA,ETF,2020-09-09,null,Active
+AAAU,Goldman Sachs Physical Gold ETF,BATS,ETF,2018-08-15,null,Active
+AACBR,Artius II Acquisition Inc Rights,NASDAQ,Stock,2025-04-07,null,Active
+AACBU,Artius II Acquisition Inc - Units (1 Ord Shs & 1 Rts),NASDAQ,Stock,2025-02-13,null,Active
+AACG,ATA Creativity Global,NASDAQ,Stock,2008-01-29,null,Active
+AACI,Armada Acquisition Corp II - Class A,NASDAQ,Stock,2025-06-24,null,Active
+AACIU,Armada Acquisition Corp II - Units (1 Ord Cls A & 1/2 War),NASDAQ,Stock,2025-05-21,null,Active
+AACIW,Armada Acquisition Corp II - Warrants(21/05/2030),NASDAQ,Stock,2025-06-24,null,Active
+''';
+}
